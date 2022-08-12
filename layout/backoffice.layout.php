2e46eb335552afca4e4da4304adc0479860e5a2e
Javier Fernández <leqjl93@hotmail.com>
2022-08-11 21:14:03 -0500

    fixing chores
    fixing chores

--- layout/backoffice.layout.php
+++ layout/backoffice.layout.php
@@ -270,7 +270,7 @@
                         <h6 class="ps-4 ms-2 text-uppercase text-xs font-weight-bolder opacity-6">Ajustes de cuenta</h6>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link  <?php if(in_array($route,[JFStudio\Router::Profile,JFStudio\Router::Range])){?>active<?php } ?>"
+                        <a class="nav-link  <?php if(in_array($route,[JFStudio\Router::Profile])){?>active<?php } ?>"
                             href="../../apps/backoffice/profile">
                             <div
                                 class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
