fdd705e94ea666375075531f3066027b9d4eeae8
Javier Fernández <leqjl93@hotmail.com>
2022-12-23 10:33:54 -0600

    abc

--- apps/signup/index.php
+++ apps/signup/index.php
@@ -1,27 +1,4 @@
-<?php define("TO_ROOT", "../..");
+<?php 
 
-require_once TO_ROOT . "/system/core.php";
-
-$UserLogin = new GranCapital\UserLogin;
-
-if($UserLogin->_loaded === true) {
-	HCStudio\Util::redirectTo(TO_ROOT . "/apps/backoffice/");
-}
- 
-$Layout = JFStudio\Layout::getInstance();
-
-$route = JFStudio\Router::Signup;
-$Layout->init(JFStudio\Router::getName($route),"index","two_columns","",TO_ROOT."/");
-
-$Layout->setScriptPath(TO_ROOT . '/src/');
-$Layout->setScript([
-	'jquery.mask.js',
-	'signup.vue.js',
-	'signup.css'
-]);
-
-$Layout->setVar([
-	'UserLogin' => $UserLogin,
-	'Country' => (new World\Country)
-]);
-$Layout();
+echo 1;die;
+?>
